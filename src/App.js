095458d7705082c982e1520b9924ba1cb5b4a88c
Afida Rindy <afidarindy3@gmail.com>
2020-03-12 23:14:11 +0700

    add image2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ const { Header, Content, Footer } = Layout;
 const { Meta } = Card;
 const cardData = [
 {
-  
   destination : "Berlin, Germany",
   img : "https://ssl.tzoo-img.com/images/tzoo.hd.14862.7444.937862.Berlin.jpg?width=392&spr=3",
   note : "Visit the Reichstag building. Admire the Berliner Dom. Time travel through East Side Gallery's paintings.",
@@ -26,6 +25,11 @@ const cardData = [
   destination : "Tokyo, Japan",
   img : "https://www.globeguide.ca/wp-content/uploads/2013/10/Japan-Tokyo-asakusa.jpg",
   note : "Snap up anime films and collectables at Akihabara. Take in old-fashioned Yanaka Ginza Shitamachi.",
+},
+{
+  destination : "Istanbul, Turkey",
+  img : "https://loveandroad.com/wp-content/uploads/2014/12/1-First-trip-to-istanbul-1280x720.jpg",
+  note : "Pay a visit to the Hagia Sophia. Head inside the Blue Mosque. Discover an epic ceiling at the Chora Church.",
 }
 ]
 
@@ -36,8 +40,8 @@ return (
   <Menu
     theme="dark"
     mode="horizontal"
-    defaultSelectedKeys={['2']}
-    style={{ lineHeight: '64px' }}
+    defaultSelectedKeys={['1']}
+    style={{ lineHeight: '50px' }}
   >
     <Menu.Item key="1">Home</Menu.Item>
     <Menu.Item key="2">About</Menu.Item>
@@ -47,11 +51,11 @@ return (
 <Content style={{ padding: '0 50px' }}>
   <Breadcrumb style={{ margin: '16px 0' }}>
     <Breadcrumb.Item>Home</Breadcrumb.Item>
-    <Breadcrumb.Item>Country</Breadcrumb.Item>
-    <Breadcrumb.Item>Bucket list</Breadcrumb.Item>
+    <Breadcrumb.Item>Popular</Breadcrumb.Item>
+    <Breadcrumb.Item>Rindy's Bucket List</Breadcrumb.Item>
   </Breadcrumb>
   <div className="site-layout-content">
-    <Row gutter={25}>
+    <Row gutter={32}>
   {cardData.map (data=>
     <Col>
     <Card bordered={false}>
