c1114c83eaccb08f8cf3fa65a23ca80b6cf144bd
Afida Rindy <afidarindy3@gmail.com>
2020-03-12 11:28:27 +0700

    site card wrapper

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-    import React from 'react';
-    //import logo from './logo.svg';
-    import './App.css';
+  import React from 'react';
+  //import logo from './logo.svg';
+  import './App.css';
 
-    import './style.css';
-    import { Row, Col, Card} from 'antd';
+  import './style.css';
+  import { Row, Col, Card} from 'antd';
 
-    function App () {
-      const cardData = [
-        {
-          country : "South Korea",
-          city : "Seoul",
-        },
-        {
-          country : "England",
-          city : "London",
-        },
-        {
-          country : "Japan",
-          city : "Tokyo",
-        }
-      ]
+  function App () {
+    const cardData = [
+      {
+        country : "South Korea",
+        city : "Seoul",
+      },
+      {
+        country : "England",
+        city : "London",
+    },
+      {
+        country : "Japan",
+        city : "Tokyo",
+      }
+    ]
 
-      return (
-        <div className="site-card-wrapper">
-        <Row gutter={16}>
-          {cardData.map (data=>
-            <Col span={8}>
-            <Card bordered={true}>
-               <h2>{data.country}</h2>
-               <p>{data.city}</p>
-            </Card>
-            </Col>
-            )}
-        </Row>
-        </div>
-        );
-    }
-    
-    export default App;
+    return (
+      <div className="site-card-wrapper">
+      <Row gutter={16}>
+        {cardData.map (data=>
+          <Col span={8}>
+          <Card bordered={true}>
+              <h2>{data.country}</h2>
+              <p>{data.city}</p>
+          </Card>
+          </Col>
+          )}
+      </Row>
+      </div>
+      );
+  }
+
+  export default App;
