2944d8a931678e805b9b27e47894e85db339e4cc
Afida Rindy <afidarindy3@gmail.com>
2020-03-30 16:57:47 +0700

    Updates

---
+++ precache-manifest.8f8c8617e19be1c4082a26a565556a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e965ab6aa26ad547f4dc4e77bfcda3",
+    "url": "/WEB3A-431729-AFIDA-REACT/index.html"
+  },
+  {
+    "revision": "2ff4b7c22b9b0d9f2ca3",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "dc2504c6effb0d8f6185",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/css/main.de750538.chunk.css"
+  },
+  {
+    "revision": "2ff4b7c22b9b0d9f2ca3",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/js/2.fc10f3c5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/js/2.fc10f3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2504c6effb0d8f6185",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/js/main.5795a828.chunk.js"
+  },
+  {
+    "revision": "8b3fa8ddc421f4b9a343",
+    "url": "/WEB3A-431729-AFIDA-REACT/static/js/runtime-main.9a416f55.js"
+  }
+]);
